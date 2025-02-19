28f7db0821248e864b164ab032d09831f0c57387
SAYEED <90544352+msayeeduddina@users.noreply.github.com>
2025-02-19 14:27:21 +0530

    password for db mysql; mac-12345678 , windows-1234

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise')
 const mysqlPool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: '12345678',
     database: 'users_db'
 })
 
