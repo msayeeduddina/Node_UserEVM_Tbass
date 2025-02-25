dd51a579c3fcc65d659b2665e907204e80870cda
msayeeduddina <msayeeduddina@gmail.com>
2025-02-25 19:22:50 +0530

    Win = password: '1234', Mac = password: '12345678',

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise')
 const mysqlPool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '12345678',
+    password: '1234',
     database: 'users_db'
 })
 
