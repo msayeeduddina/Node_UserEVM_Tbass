19be7faac934cd9241b51f02ff502e0a8896ba82
Sayeed-Tribox <sayee.duddin@tribox.me>
2025-03-16 09:41:12 -0700

    .env to connect create pool

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require('mysql2/promise')
 
 const mysqlPool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'users_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA,
+    port: process.env.DB_PORT,
 })
 
 
